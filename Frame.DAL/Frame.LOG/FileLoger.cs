ee63164f6b5b2c702b29f210f67b3b9d5ec604fa
swfightforyl <swfightforyl@163.com>
2016-06-28 20:48:53 +0800

    logger

---
+++ Frame.DAL/Frame.LOG/FileLoger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frame.LOG
+{
+    public class FileLoger : ILoger
+    {
+        public void WriteLog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Frame.DAL/Frame.LOG/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frame.LOG
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error,
+        Fatar
+    }
+}
